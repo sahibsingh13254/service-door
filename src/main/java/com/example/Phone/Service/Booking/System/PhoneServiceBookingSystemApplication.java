a4793699c414e6bad94f70422de0e12e19b5eac3
sahibsingh13254 <sahibs98765432@gmail.com>
2025-08-22 12:21:14 +0530

    Added directory and liquibase

---
+++ src/main/java/com/example/Phone/Service/Booking/System/PhoneServiceBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Phone.Service.Booking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneServiceBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneServiceBookingSystemApplication.class, args);
+	}
+
+}
